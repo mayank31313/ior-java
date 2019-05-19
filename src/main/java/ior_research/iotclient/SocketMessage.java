334fe4ede5dd3e327c7414f230b672986730dc30
Mayank Shinde <jacob8547black@gmail.com>
2019-05-20 00:27:09 +0530

    README edited

--- src/main/java/ior_research/iotclient/SocketMessage.java
+++
@@ -1,9 +0,0 @@
-package ior_research.iotclient;
-
-import java.util.HashMap;
-
-public class SocketMessage{
-    public String message;
-    public Status status;
-    public HashMap<String,String> syncData;
-}
--- src/main/java/ior_research/iotclient/Status.java
+++
@@ -1,5 +0,0 @@
-package ior_research.iotclient;
-
-public enum Status{
-    Initialized,Connected,Disconnected,ERROR,Operation,SYNC
-}
